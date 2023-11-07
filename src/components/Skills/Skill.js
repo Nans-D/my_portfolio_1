55fea2be0f90006da4f83a51850dd539b701e84f
Nans-D <ndelaubert@gmail.com>
2023-11-07 20:58:12 +0100

    skills / btn modifications

--- src/components/Skills/Skill.js
+++ src/components/Skills/Skill.js
@@ -1,82 +1,64 @@
 import './Skill.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHtml5 } from "@fortawesome/free-brands-svg-icons"
+import { faHtml5, faReact } from "@fortawesome/free-brands-svg-icons"
 import { faCss3Alt } from "@fortawesome/free-brands-svg-icons"
 import { faJs } from "@fortawesome/free-brands-svg-icons"
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faServer } from '@fortawesome/free-solid-svg-icons'
 import { faNodeJs } from '@fortawesome/free-brands-svg-icons'
 import { faBook } from '@fortawesome/free-solid-svg-icons'
 import { faCalendarXmark } from '@fortawesome/free-solid-svg-icons'
 
 function Skill(){
     return <div>
-        <div className="skills-container">
             <h1 id='skill-title'>Skills</h1>
-            <div className='language-container'>
-                <div className='language-container-child'>
-                    <h3 className='front-end-text'>Front-end</h3>
-                    <div className='icon-container'>
-                        <div className='cards-container bg-orange'>
+        <div className="skills-container">
+            <div className='front-end-container'>
+                <p className='skills-title'>Front-end</p>
+                    <ul className='cards-container'>
+                        <li className='icons-container'>
                             <FontAwesomeIcon icon={faHtml5} />
-                            <div>
-                                <p className='p-html'>HTLM</p>
-                            </div>
-                        </div>
-                        <div className='cards-container bg-blue'>
                             <FontAwesomeIcon icon={faCss3Alt} />
-                            <div>
-                                <p className='p-css'>CSS</p>
-                            </div>
-                        </div>
-                        <div className='cards-container'>
                             <FontAwesomeIcon icon={faJs} />
-                            <div>
-                                <p className='p-js'>JavaScript</p>
-                            </div>
-                        </div>
-                    </div>
+                            <FontAwesomeIcon icon={faReact} />
+                        </li>
+                        <li className='text-icons-container'>
+                            <p className='p-html'>HTLM</p>
+                            <p className='p-html'>CSS</p>
+                            <p className='p-html'>JavaScript</p>
+                            <p className='p-html'>React.js</p>
+                        </li>
+                    </ul>
                 </div>
-                <div className='back-end-softskills-container'>
-                    <div className='language-container-child'>
-                        <h3 className='back-end-text'>Back-end</h3>
-                        <div className='icon-container'>
-                            <div className='cards-container'>
-                                <FontAwesomeIcon icon={faNodeJs} />
-                                <div>
-                                    <p className='p-js'>NodeJS</p>
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <div className='language-container-child'>
-                        <h3 className='soft-skills-text'>SoftSkills</h3>
-                        <div className='icon-container'>
-                            <div className='cards-container'>
-                                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                                <div>
-                                    <p className='p-js'>Curious</p>
-                                </div>
-                            </div>
-                            <div className='cards-container'>
-                                <FontAwesomeIcon icon={faBook} />
-                                <div>
-                                    <p className='p-js'>Reader</p>
-                                </div>
-                            </div>
-                            <div className='cards-container'>
-                                <FontAwesomeIcon icon={faCalendarXmark} />
-                                <div>
-                                    <p className='p-js'>Schedule</p>
-                                </div>
-                            </div>
-                                
-                        </div>
-                    </div> 
+            <div className='back-end-container'>
+                <p className='skills-title'>Back-end</p>
+                    <ul className='cards-container'>
+                        <li className='icons-container'>
+                            <FontAwesomeIcon icon={faNodeJs} />
+                            <FontAwesomeIcon icon={faServer} />
+                        </li>
+                        <li className='text-icons-container'>
+                            <p className='p-html'>Node.js</p>
+                            <p className='p-html'>REST API</p>
+                        </li>
+                    </ul>
+                </div>
+            <div className='back-end-container'>
+                <p className='skills-title'>Softskills</p>
+                    <ul className='cards-container'>
+                        <li className='icons-container'>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            <FontAwesomeIcon icon={faBook} />
+                            <FontAwesomeIcon icon={faCalendarXmark} />
+                        </li>
+                        <li className='text-icons-container'>
+                            <p className='p-html'>Clever</p>
+                            <p className='p-html'>Reader</p>
+                            <p className='p-html'>Schedule</p>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
-</div>
 }
 
 export default Skill
